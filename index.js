2655f55c921bd6c5d93947d07a095c8b2682f9c7
Reinoptland <reinoptland@gmail.com>
2019-11-05 17:32:37 +0100

    Initial commit with todolist

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello')
+
+
+// TODO:
+
+// X install dependencies
+// - run docker with postgres
+// - create server
+// - Create models
+// - sync with database
+// - configure body parser
+// - GET
+// - POST
+// - PUT
+// - DELETE
